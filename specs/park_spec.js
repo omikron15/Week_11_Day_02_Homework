b3036734071418d220754dd163d87f338087b66e
omikron15 <omikron15@hotmail.co.uk>
2018-06-05 17:24:00 +0100

    Adds setup code for park tests

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -1,2 +1,17 @@
 const assert = require('assert')
 const Park = require('../models/park.js')
+const Park = require('../models/dinosaur.js')
+
+describe("Dinosaur", function(){
+
+  let park;
+  let dinosaur1;
+  let dinosaur2;
+  let dinosaur3;
+
+  beforeEach(function(){
+    park = new Park();
+    dinosaur1 = new Dinosaur("Tyrannosaurus", 5);
+    dinosaur2 = new Dinosaur("Velociraptor", 1);
+    dinosaur3 = new Dinosaur("Triceratops", 3);
+  });
