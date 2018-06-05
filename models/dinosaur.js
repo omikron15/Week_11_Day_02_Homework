c36367ecd9f2b01c41168539b764eae10d490979
omikron15 <omikron15@hotmail.co.uk>
2018-06-05 17:14:46 +0100

    adds Dinosaur constructor with basic attributes

---
+++ models/dinosaur.js
@@ -0,0 +1,6 @@
+const Dinosaur = function(type, noOfOffspring){
+  this.type = type;
+  this.noOfOffspring = noOfOffspring;
+}
+
+module.exports = Dinosaur;
--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -1,2 +1,20 @@
 const assert = require('assert')
 const Dinosaur = require('../models/dinosaur.js')
+
+describe("Dinosaur", function(){
+
+  let dinosaur;
+
+  beforeEach(function(){
+    dinosaur = new Dinosaur("Tyrannosaurus", 5);
+  });
+
+  it("Dinosaur has type", function(){
+    assert.strictEqual(dinosaur.type, "Tyrannosaurus");
+  });
+
+  it("Dinosaur has number of offspring", function(){
+    assert.strictEqual(dinosaur.noOfOffspring, 5);
+  });
+
+})
